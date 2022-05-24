df9ff4718c90165282f616a1fcf5d28e6f7b25f3
OMiD <o.rasouli92@gmail.com>
2022-05-24 20:55:59 +0430

    Add Menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,6 @@
+export function Menu() {
+  return (
+    <div>
+    </div>
+  );
+}
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -4,6 +4,7 @@ import { Menu } from "../../components/Menu/Menu";
 export function MainMenu() {
   return (
     <Layout>
+      <Menu />
     </Layout>
   );
 }

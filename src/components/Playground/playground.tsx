c5c4a06e994d09743bb88025696aef7ca4fefb63
OMiD <o.rasouli92@gmail.com>
2022-05-24 20:26:37 +0430

    Refactor Playground

--- src/components/Playground/playground.tsx
+++ src/components/Playground/playground.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Timer from "../Timer/Timer";
 import HighOrLow from "../Games/HighOrLow/HighOrLow";
 import FlickMaster from "../Games/FlickMaster/FlickMaster";
@@ -5,24 +6,31 @@ import FollowTheLeader from "../Games/FollowTheLeader/FollowTheLeader";
 import UnfollowTheLeader from "../Games/UnfollowTheLeader/UnfollowTheLeader";
 import style from "./playground.module.scss";
 
-import {
-  ConditionalGameRender,
-  Condition,
-} from "../ConditionalRender/ConditionalGameRender";
-
-export function PlayGround() {
+export function PlayGround(props: { game: string }) {
+  const [choosenGame] = useState<string>(props.game);
   const time = new Date();
   time.setSeconds(time.getSeconds() + 10);
+
+  function RenderGame(game: string) {
+    switch (game) {
+      case "HighOrLow":
+        return <HighOrLow count={60} />;
+      case "FlickMaster":
+        return <FlickMaster count={60} />;
+      case "FollowTheLeader":
+        return <FollowTheLeader count={60} />;
+      case "UnfollowTheLeader":
+        return <UnfollowTheLeader count={60} />;
+
+      default:
+        return <></>;
+    }
+  }
 
   return (
     <div className={style.playground}>
       <Timer expiryTimestamp={time} />
-      <ConditionalGameRender game="HighOrLow">
-        <Condition game={HighOrLow} count={60} />
-        <Condition game={FlickMaster} count={60} />
-        <Condition game={FollowTheLeader} count={60} />
-        <Condition game={UnfollowTheLeader} count={60} />
-      </ConditionalGameRender>
+      {RenderGame(choosenGame)}
     </div>
   );
 }

4170ea502a36dd0592150d84e903ea6e13701af5
OMiD <o.rasouli92@gmail.com>
2022-05-25 09:59:29 +0430

    Add sad component to quit page

--- src/pages/quit/index.tsx
+++ src/pages/quit/index.tsx
@@ -1,8 +1,10 @@
 import { Layout } from "../../components/Layout/Layout";
+import { Sad } from "../../components/Sad/Sad";
 
 export default function index() {
   return (
     <Layout>
+      <Sad />
     </Layout>
   );
 }

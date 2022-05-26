adc7733b8581bbaa30d282ae70878e01635c956a
OMiD <o.rasouli92@gmail.com>
2022-05-26 15:09:29 +0430

    Add game object to Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,27 @@
 import Link from "next/link";
 
 export function Menu() {
+  const games = [
+    {
+      key: "fkm",
+      name: "Flick Master",
+      id: "FlickMaster",
+      class: "flickMaster",
+    },
+    {
+      key: "ftl",
+      name: "Follow The Leader",
+      id: "FollowTheLeader",
+      class: "followTheLeader",
+    },
+    { key: "hol", name: "High or Low", id: "HighOrLow", class: "highOrLow" },
+    {
+      key: "uftl",
+      name: "Unfollow the Leader",
+      id: "UnfollowtheLeader",
+      class: "unfollowTheLeader",
+    },
+  ];
   return (
     <div>
       <Link href={""}>

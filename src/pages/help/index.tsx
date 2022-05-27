7093b673451be4962b214c588055ef2b6194d6ab
OMiD <o.rasouli92@gmail.com>
2022-05-27 15:59:54 +0430

    Add help page

---
+++ src/pages/help/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../../components/Layout/Layout";
+import Help from "../../components/Help/Help";
+
+export default function HelpPage() {
+  return (
+    <Layout>
+      <Help />
+    </Layout>
+  );
+}
